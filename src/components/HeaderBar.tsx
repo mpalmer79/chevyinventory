aa2a7d4475806ad6f11b1ed8021fc6e4745f36b6
mpalmer79 <mpalm2025@gmail.com>
2026-01-30 10:40:21 -0500

    Update HeaderBar.tsx

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -9,9 +9,9 @@ interface Props {
 export const HeaderBar: FC<Props> = () => {
   return (
     <header className="relative overflow-hidden">
-      {/* Hero Image */}
+      {/* Hero Image - reduced height on mobile */}
       <div 
-        className="relative h-[380px] md:h-[380px] sm:h-[220px] bg-cover bg-top bg-no-repeat"
+        className="relative h-[200px] sm:h-[280px] md:h-[380px] bg-cover bg-center bg-no-repeat"
         style={{ backgroundImage: "url('/chevy-showroom.png')" }}
       >
         {/* Light overlay */}
@@ -19,7 +19,7 @@ export const HeaderBar: FC<Props> = () => {
         
         {/* Bottom fade gradient */}
         <div 
-          className="absolute bottom-0 left-0 right-0 h-[150px]"
+          className="absolute bottom-0 left-0 right-0 h-[80px] sm:h-[120px] md:h-[150px]"
           style={{ 
             background: "linear-gradient(to bottom, transparent 0%, hsl(var(--background)) 100%)" 
           }} 
